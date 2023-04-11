3356766bcd398bb535ea82be44dda789fc0f3a30
Ariane Kevinny <51418537+ArianeKevinny@users.noreply.github.com>
2023-04-11 12:56:02 -0300

    Create plota.py

---
+++ plota.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# Criando um gráfico com os eixos coordenados
+
+def plota(f,a,b):
+  fig, ax = plt.subplots()
+  ax.set_ylabel('f(x)')
+  ax.set_xlabel('x')
+  ax.grid()
+  
+  #https://stackoverflow.com/questions/25689238/,→show-origin-axis-x-y-in-matplotlib-plot
+  # set the x-spine (see below for more info on `set_position`)
+  
+  plt.axhline(0, color='black', linewidth=1)
+  
+  x = np.linspace(a,b,100)
+  y = f(x)
+  ax.plot(x,y)
+  plt.xlim([a, b])
+  plt.ylim([y.min(), y.max()])
+  plt.show()
+  
+def f(x):
+  return 2.0 * x * np.cos(2.0*x) - (x + 1)**2
+
+plota(f, 0, 100)
